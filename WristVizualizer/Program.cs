a7b2bee79cda7ba8210cebd97b54d077a7c6e9a9
Evan Leventhal <evan@networkingdoc.com>
2007-04-20 14:47:53 +0000

    Added exception catching to the main application for logging purposes.

--- WristVizualizer/Program.cs
+++ WristVizualizer/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
+using System.IO;
 
 namespace WristVizualizer
 {
@@ -14,7 +15,29 @@ namespace WristVizualizer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            Application.ThreadException += new System.Threading.ThreadExceptionEventHandler(Application_ThreadException);
             Application.Run(new WristVizualizer());
+
+        }
+
+        static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+
+
+            string log = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "error.log");
+            //log = @"C:\Documents and Settings\Evan\My Documents\Visual Studio 2005\Projects\Vizualization\WristVizualizer\bin\Release\test.txt";
+            try //there is a chance of failure when writing to the log file
+            {
+                StreamWriter wr = new StreamWriter(log, true);
+                wr.WriteLine(e.Exception.ToString());
+                wr.Close();
+                wr.Dispose();
+            }
+            catch { } //if the log fails....well, we are just fucked then
+
+            //throw ex; //rethrow original exception
+            MessageBox.Show(e.Exception.Message,"Fatal Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            Application.Exit();
         }
     }
 }
