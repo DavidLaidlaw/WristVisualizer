0331c1530aa6f7334145b5722a0ef108041678b7
Evan Leventhal <evan@networkingdoc.com>
2007-04-23 06:23:09 +0000

    Ugly hack to fix motion files, to remove motion of MC's. Also updated the version number.

--- WristVizualizer/HelpAbout.Designer.cs
+++ WristVizualizer/HelpAbout.Designer.cs
@@ -44,16 +44,16 @@ namespace WristVizualizer
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(10, 7);
+            this.pictureBox1.Location = new System.Drawing.Point(5, 1);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(110, 130);
+            this.pictureBox1.Size = new System.Drawing.Size(110, 127);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // buttonOK
             // 
             this.buttonOK.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.buttonOK.Location = new System.Drawing.Point(325, 103);
+            this.buttonOK.Location = new System.Drawing.Point(318, 84);
             this.buttonOK.Name = "buttonOK";
             this.buttonOK.Size = new System.Drawing.Size(89, 31);
             this.buttonOK.TabIndex = 1;
@@ -64,7 +64,7 @@ namespace WristVizualizer
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(126, 9);
+            this.label1.Location = new System.Drawing.Point(124, 3);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(193, 29);
             this.label1.TabIndex = 2;
@@ -74,16 +74,16 @@ namespace WristVizualizer
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(130, 47);
+            this.label2.Location = new System.Drawing.Point(130, 32);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(84, 16);
             this.label2.TabIndex = 3;
-            this.label2.Text = "Version 1.0.1";
+            this.label2.Text = "Version 1.0.2";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(130, 72);
+            this.label3.Location = new System.Drawing.Point(130, 57);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(28, 13);
             this.label3.TabIndex = 4;
@@ -93,7 +93,7 @@ namespace WristVizualizer
             // 
             this.linkLabelEmail.AutoSize = true;
             this.linkLabelEmail.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
-            this.linkLabelEmail.Location = new System.Drawing.Point(164, 72);
+            this.linkLabelEmail.Location = new System.Drawing.Point(164, 57);
             this.linkLabelEmail.Name = "linkLabelEmail";
             this.linkLabelEmail.Size = new System.Drawing.Size(119, 13);
             this.linkLabelEmail.TabIndex = 5;
@@ -104,7 +104,7 @@ namespace WristVizualizer
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(145, 92);
+            this.label4.Location = new System.Drawing.Point(145, 77);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(109, 13);
             this.label4.TabIndex = 6;
@@ -114,7 +114,7 @@ namespace WristVizualizer
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(131, 89);
+            this.label5.Location = new System.Drawing.Point(131, 74);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(19, 18);
             this.label5.TabIndex = 7;
@@ -123,7 +123,7 @@ namespace WristVizualizer
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(132, 110);
+            this.label6.Location = new System.Drawing.Point(132, 95);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(86, 13);
             this.label6.TabIndex = 8;
@@ -135,7 +135,7 @@ namespace WristVizualizer
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.buttonOK;
-            this.ClientSize = new System.Drawing.Size(433, 145);
+            this.ClientSize = new System.Drawing.Size(433, 131);
             this.ControlBox = false;
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label4);
@@ -149,6 +149,7 @@ namespace WristVizualizer
             this.Name = "HelpAbout";
             this.ShowInTaskbar = false;
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "About Wrist Visualizer";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
