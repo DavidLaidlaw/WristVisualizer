2ed39158eceb87d6de6a3be0020058bd87088a38
Evan Leventhal <evan@networkingdoc.com>
2008-05-20 08:12:09 +0000

    Added new (untested) method for rotating the camera, based on SoGuiExaminerViewerP::rotXWheelMotion(float value, float oldvalue) in coin

--- libCoin3D/Camera.cpp
+++ libCoin3D/Camera.cpp
@@ -5,3 +5,34 @@ libCoin3D::Camera::Camera()
 {
 	_node = new SoOrthographicCamera();
 }
+
+void libCoin3D::Camera::rotateCameraInX(const float movement)
+{
+	this->rotateCamera(SbVec3f(-1, 0, 0), movement);
+}
+
+void libCoin3D::Camera::rotateCameraInY(const float movement)
+{
+	this->rotateCamera(SbVec3f(0, -1, 0), movement);
+}
+
+void libCoin3D::Camera::rotateCamera(const SbVec3f & aroundaxis, const float delta)
+{
+	SoCamera* cam = (SoOrthographicCamera*)_node;
+
+	const SbVec3f DEFAULTDIRECTION(0, 0, -1);
+	const SbRotation currentorientation = cam->orientation.getValue();
+
+	SbVec3f currentdir;
+	currentorientation.multVec(DEFAULTDIRECTION, currentdir);
+
+	const SbVec3f focalpoint = cam->position.getValue() +
+		cam->focalDistance.getValue() * currentdir;
+	
+	// set new orientation
+	cam->orientation = SbRotation(aroundaxis, delta) * currentorientation;
+
+	SbVec3f newdir;
+	cam->orientation.getValue().multVec(DEFAULTDIRECTION, newdir);
+	cam->position = focalpoint - cam->focalDistance.getValue() * newdir;
+}
--- libCoin3D/Camera.h
+++ libCoin3D/Camera.h
@@ -11,7 +11,12 @@ public ref class Camera : Node
 public:
 	Camera();
 	virtual SoNode* getNode() override { return _node; }
+
+	void rotateCameraInX(const float movement);
+	void rotateCameraInY(const float movement);
 private:
+	void rotateCamera(const SbVec3f & aroundaxis, const float delta);
+
 	SoNode* _node;
 };
 }
