c112f219fc01a693475454a1eb30b90a7e008097
Evan Leventhal <evan@networkingdoc.com>
2008-07-18 06:18:39 +0000

    Moved code location around

--- WristVizualizer/RegistrySettings.cs
+++ WristVizualizer/RegistrySettings.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
 using Microsoft.Win32;
 
 namespace WristVizualizer
@@ -12,7 +13,6 @@ namespace WristVizualizer
         private const string RECENT_FILES_KEY = "RecentFiles";
         private const string FILENAME_NAME = "Filename";
 
-        //static RegistryKey a = Registry.CurrentUser.OpenSubKey(
         public static string getSettingString(string name)
         {
             RegistryKey root = Registry.CurrentUser.OpenSubKey(String.Format("Software\\{0}\\{1}", Application.CompanyName, Application.ProductName));
@@ -102,6 +102,123 @@ namespace WristVizualizer
             RegistryKey key = Registry.CurrentUser.CreateSubKey(String.Format("Software\\{0}\\{1}\\{2}\\{3}",
                 Application.CompanyName, Application.ProductName, RECENT_FILES_KEY, index));
             key.SetValue(FILENAME_NAME, filename, RegistryValueKind.String);
+        }
+
+
+        /// <summary>
+        /// Shortens a pathname by either removing consecutive components of a path
+        /// and/or by removing characters from the end of the filename and replacing
+        /// then with three elipses (...)
+        ///
+        /// In all cases, the root of the passed path will be preserved in it's entirety.
+        ///
+        /// If a UNC path is used or the pathname and maxLength are particularly short,
+        /// the resulting path may be longer than maxLength.
+        ///
+        /// This method expects fully resolved pathnames to be passed to it.
+        /// (Use Path.GetFullPath() to obtain this.)
+        /// </summary>
+        /// <param name="pathname"></param>
+        /// <param name="maxLength"></param>
+        /// <returns></returns>
+        public static string ShortenPathname(string pathname, int maxLength)
+        {
+            if (pathname.Length <= maxLength)
+                return pathname;
+
+            string root = Path.GetPathRoot(pathname);
+            if (root.Length > 3)
+                root += Path.DirectorySeparatorChar;
+
+            string[] elements = pathname.Substring(root.Length).Split(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
+
+            int filenameIndex = elements.GetLength(0) - 1;
+
+            if (elements.GetLength(0) == 1) // pathname is just a root and filename
+            {
+                if (elements[0].Length > 5) // long enough to shorten
+                {
+                    // if path is a UNC path, root may be rather long
+                    if (root.Length + 6 >= maxLength)
+                    {
+                        return root + elements[0].Substring(0, 3) + "...";
+                    }
+                    else
+                    {
+                        return pathname.Substring(0, maxLength - 3) + "...";
+                    }
+                }
+            }
+            else if ((root.Length + 4 + elements[filenameIndex].Length) > maxLength) // pathname is just a root and filename
+            {
+                root += "...\\";
+
+                int len = elements[filenameIndex].Length;
+                if (len < 6)
+                    return root + elements[filenameIndex];
+
+                if ((root.Length + 6) >= maxLength)
+                {
+                    len = 3;
+                }
+                else
+                {
+                    len = maxLength - root.Length - 3;
+                }
+                return root + elements[filenameIndex].Substring(0, len) + "...";
+            }
+            else if (elements.GetLength(0) == 2)
+            {
+                return root + "...\\" + elements[1];
+            }
+            else
+            {
+                int len = 0;
+                int begin = 0;
+
+                for (int i = 0; i < filenameIndex; i++)
+                {
+                    if (elements[i].Length > len)
+                    {
+                        begin = i;
+                        len = elements[i].Length;
+                    }
+                }
+
+                int totalLength = pathname.Length - len + 3;
+                int end = begin + 1;
+
+                while (totalLength > maxLength)
+                {
+                    if (begin > 0)
+                        totalLength -= elements[--begin].Length - 1;
+
+                    if (totalLength <= maxLength)
+                        break;
+
+                    if (end < filenameIndex)
+                        totalLength -= elements[++end].Length - 1;
+
+                    if (begin == 0 && end == filenameIndex)
+                        break;
+                }
+
+                // assemble final string
+                for (int i = 0; i < begin; i++)
+                {
+                    root += elements[i] + '\\';
+                }
+
+                root += "...\\";
+
+                for (int i = end; i < filenameIndex; i++)
+                {
+                    root += elements[i] + '\\';
+                }
+
+                return root + elements[filenameIndex];
+            }
+            return pathname;
         }
     }
 }
