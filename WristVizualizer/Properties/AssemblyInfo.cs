dd2e79fb4d5442c4fc1b5735572e262f14e99f9f
Evan Leventhal <evan@networkingdoc.com>
2008-12-27 04:46:49 +0000

    update version number

--- WristVizualizer/Properties/AssemblyInfo.cs
+++ WristVizualizer/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.7.2.*")]
+[assembly: AssemblyVersion("1.7.3.*")]
 
 //Aditional settings for Mono.GetOptions.dll 
 [assembly: Mono.UsageComplement("[filename1] [filename2] ...")]
