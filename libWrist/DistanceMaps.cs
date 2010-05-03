3b2ff4a480065e028e80c1090cb932af79528369
Evan Leventhal <evan@networkingdoc.com>
2010-05-03 18:27:25 +0000

    Deleted unused code

--- libWrist/DistanceMaps.cs
+++ libWrist/DistanceMaps.cs
@@ -92,69 +92,6 @@ namespace libWrist
             return distances;
         }
 
-        /// <summary>
-        /// Can be used to create the distance map for a single reference bone, against a single 
-        /// test distance field.
-        /// </summary>
-        /// <param name="testDistanceField">Distance field of the test bone.</param>
-        /// <param name="referenceBone">The bone file of the reference bone. The distance map should be drawn on this bone</param>
-        /// <param name="relativeMotion">The relative transform of to move the reference bone into the distance field for the test bone</param>
-        /// <returns>an array of distance values (in mm) for each vertex of the referenceBone</returns>
-        public static double[] createDistanceMap(CTmri testDistanceField, ColoredBone referenceBone, TransformMatrix relativeMotion)
-        {
-            /* Yes this is bad, its basically just cut and paste code from above, 
-             * but I need these both to be pretty optimized, and it was hard to remove
-             * the loops from above, or I would have really had to force this
-             */
-
-            CTmri mri = testDistanceField;
-            float[,] pts = referenceBone.getVertices();
-            int numVertices = pts.GetLength(0);
-
-            double[] distances = new double[numVertices];
-
-            bool isNeutral = relativeMotion.isIdentity();
-
-            //for each vertex           
-            for (int i = 0; i < numVertices; i++)
-            {
-                distances[i] = Double.MaxValue; //set this vertex to the default
-
-                double x = pts[i, 0];
-                double y = pts[i, 1];
-                double z = pts[i, 2];
-
-                //check if we need to move for non neutral position
-                if (!isNeutral)
-                {
-                    //lets move the bone getting colored, into the space of the other bone...
-                    double[] p0 = new double[] { x, y, z };
-                    double[] p1 = relativeMotion * p0;
-                    x = p1[0];
-                    y = p1[1];
-                    z = p1[2];
-                }
-
-                double dX = (x - mri.CoordinateOffset[0]) / mri.voxelSizeX;
-                double dY = (y - mri.CoordinateOffset[1]) / mri.voxelSizeY;
-                double dZ = (z - mri.CoordinateOffset[2]) / mri.voxelSizeZ;
-
-                const double xBound = 96.9; //get the boundaries of the distance cube
-                const double yBound = 96.9; //
-                const double zBound = 96.9; //
-
-                ////////////////////////////////////////////////////////
-                //is surface point picked inside of the cube?
-
-                if (dX >= 3.1 && dX <= xBound && dY >= 3.1
-                    && dY <= yBound && dZ >= 3.1 && dZ <= zBound)
-                {
-                    distances[i] = mri.sample_s_InterpCubit(dX, dY, dZ);
-                }
-            }
-            return distances;
-        }
-
         public static int[] createColormap(double[] distances, double maxColorDistance)
         {
             int numVertices = distances.Length;
@@ -209,23 +146,6 @@ namespace libWrist
             m.setColor(0, 1, 0);
             return spher1;
         }
-
-        //[Obsolete("Don't f'ing use!")]
-        //public void createContourShit()
-        //{
-        //    double[] cDistances = new double[] { 1.0, 2.0 };
-        //    for (int i = 0; i < Wrist.NumBones; i++)
-        //    {
-        //        Contour cont1 = createContourSingleBoneSinglePosition(i, 0, cDistances);
-        //        _colorBones[i].setAndReplaceContour(cont1);
-        //        for (int j=0; j<cDistances.Length; j++)
-        //        {
-        //            double d = cDistances[j];
-        //            Console.WriteLine("Bone {0}, contour {1}mm: Area={2}, Centroid=({3}, {4}, {5})",
-        //                i, d, cont1.Areas[j], cont1.Centroids[j][0], cont1.Centroids[j][1], cont1.Centroids[j][2]);
-        //        }
-        //    }
-        //}
 
 
         /// <summary>
