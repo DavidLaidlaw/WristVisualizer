4e6832d3a9f9b26f1a74027360592c3156248233
Dimitri Panagos <dpanagos@gnosyssystems.com>
2018-11-05 22:56:20 -0500

    Created an advanced installer project for WristVisualizer.  Updated version to 1.8.3

--- WristVizualizer/Properties/AssemblyInfo.cs
+++ WristVizualizer/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.8.2.*")]
+[assembly: AssemblyVersion("1.8.3.*")]
 
 //Aditional settings for Mono.GetOptions.dll 
 [assembly: Mono.UsageComplement("[filename1] [filename2] ...")]
